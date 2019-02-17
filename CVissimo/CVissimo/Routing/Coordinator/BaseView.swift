e98115a114eda5613f574b2e79fb6fab58f4f2e6
Pavel Shatalov <shatalovp@gmail.com>
2019-02-17 15:01:00 +0700

    COORDINATOR
    Add base coordinator entities

---
+++ CVissimo/CVissimo/Routing/Coordinator/BaseView.swift
@@ -0,0 +1,9 @@
+//
+//  BaseView.swift
+//  CVissimo
+//
+//  Created by Pavel Shatalov on 17/02/2019.
+//  Copyright © 2019 Pavel Shatalov. All rights reserved.
+//
+
+import Foundation
---
+++ CVissimo/CVissimo/Routing/Coordinator/Coordinator.swift
@@ -0,0 +1,9 @@
+//
+//  Coordinator.swift
+//  CVissimo
+//
+//  Created by Pavel Shatalov on 17/02/2019.
+//  Copyright © 2019 Pavel Shatalov. All rights reserved.
+//
+
+import Foundation
