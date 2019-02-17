43eb569045b97f019e0f3ad7a1e5c13ff739d8bf
Pavel Shatalov <shatalovp@gmail.com>
2019-02-17 15:13:00 +0700

    APPLICATION COORDINATOR: Add main entry point

--- CVissimo/CVissimo/Application/AppDelegate.swift
+++ CVissimo/CVissimo/Application/AppDelegate.swift
@@ -13,9 +13,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
+  private let rootController: UINavigationController = UINavigationController()
+  private lazy var applicaitonCoordinator: ApplicationCoordinator = {
+    let router = BaseRouter(rootController: rootController)
+    let coordinator = ApplicationCoordinator(router: router)
+    return coordinator
+  }()
+  
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    setupWindow()
+    applicaitonCoordinator.start()
     return true
   }
 
+  private func setupWindow() {
+    let window = UIWindow()
+    window.backgroundColor = .black
+    window.rootViewController = rootController
+    window.makeKeyAndVisible()
+    self.window = window
+  }
 }
 
---
+++ CVissimo/CVissimo/Application/ApplicationCoordinator.swift
@@ -0,0 +1,20 @@
+//
+//  ApplicationCoordinator.swift
+//  CVissimo
+//
+//  Created by Pavel Shatalov on 17/02/2019.
+//  Copyright © 2019 Pavel Shatalov. All rights reserved.
+//
+
+import Foundation
+
+final class ApplicationCoordinator: BaseCoordinator {
+  
+  private let router: Router
+  
+  init(router: Router) {
+    self.router = router
+  }
+  
+  
+}
