bd76c1d350e7e4a1c429b1b1e2d49bd574082245
Pavel Shatalov <shatalovp@gmail.com>
2019-02-19 13:53:46 +0700

    Add logo

---
+++ CVissimo/CVissimo/Flows/Main/MainCoordinator.swift
@@ -0,0 +1,13 @@
+//
+//  MainCoordinator.swift
+//  CVissimo
+//
+//  Created by Pavel Shatalov on 19/02/2019.
+//  Copyright © 2019 Pavel Shatalov. All rights reserved.
+//
+
+import Foundation
+
+final class MainCoordinator: BaseCoordinator {
+  
+}
--- CVissimo/CVissimoTests/Core components tests/UserDefaultsStorageTests.swift
+++ CVissimo/CVissimoTests/Core components tests/UserDefaultsStorageTests.swift
@@ -14,11 +14,9 @@ class UserDefaultsStorageTests: XCTestCase {
   private let storage: KeyValueStorage = UserDefaultsStorage()
 
   override func setUp() {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
   }
 
   override func tearDown() {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
   }
 
   func testSaveValueForKey() {
